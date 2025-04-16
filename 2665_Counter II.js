5567eef3ea8f69fafc646a0e6b499985b771b26f
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-04-16 14:38:58 +0530

    Create counter #2

---
+++ 2665_Counter II.js
@@ -0,0 +1,22 @@
+var createCounter = function(init) {
+    var current = init;
+    return {
+        increment: function() {
+            current += 1;
+            return current;
+        },
+        decrement: function() {
+            current -= 1;
+            return current;
+        },
+        reset: function() {
+            current = init;
+            return current;
+        }
+    };
+};
+
+const counter = createCounter(5)
+console.log(counter.increment()); // 6
+console.log(counter.reset()); // 5
+console.log(counter.decrement()); // 4
